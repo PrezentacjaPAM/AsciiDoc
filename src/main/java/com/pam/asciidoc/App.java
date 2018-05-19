352402c8d407046b9290b12e7a4107a52485d154
SharpShooter <b.ujazdowski@gmail.com>
2018-05-19 11:24:24 +0200

    Usuniecie zbednych rzeczy i dodanie sekcji do dokumentacji javadoc

--- src/main/java/com/pam/asciidoc/App.java
+++ src/main/java/com/pam/asciidoc/App.java
@@ -2,8 +2,13 @@ package com.pam.asciidoc;
 
 /**
  * = Klasa App
- * Tutaj wszystko się zaczęło :D
  * 
+ * === Sec 1
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut facilisis erat. Donec aliquam eget sapien fermentum iaculis. Phasellus laoreet dolor et varius tristique. Mauris semper ipsum nulla, a imperdiet metus porta sed. Vestibulum sit amet viverra mauris. Maecenas accumsan neque nec nibh venenatis tincidunt. Sed aliquam augue nec lectus egestas, ut molestie nisi lobortis. Curabitur vestibulum imperdiet velit, in blandit eros bibendum eu. Ut sed mauris a purus accumsan molestie non sed risus. Vivamus eget massa lectus. Donec sit amet elementum massa.
+ *
+ * === Sec 2
+ * Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ut facilisis erat. Donec aliquam eget sapien fermentum iaculis. Phasellus laoreet dolor et varius tristique. Mauris semper ipsum nulla, a imperdiet metus porta sed. Vestibulum sit amet viverra mauris. Maecenas accumsan neque nec nibh venenatis tincidunt. Sed aliquam augue nec lectus egestas, ut molestie nisi lobortis. Curabitur vestibulum imperdiet velit, in blandit eros bibendum eu. Ut sed mauris a purus accumsan molestie non sed risus. Vivamus eget massa lectus. Donec sit amet elementum massa.
+ *
  * @author https://github.com/PrezentacjaPAM
  */
 public class App {	
